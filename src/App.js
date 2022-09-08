3af1c77a73e4aa820a8f8023a4e7ef65e3bf5d99
RadimKotajny <KotajnyRadim@sps-karvina.cz>
2022-09-08 21:50:43 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="">
-
-    </div>
-  );
+    return (
+        <div className="text-center mt-10 text-3xl">
+            <h1 className="underline underline-offset-8 decoration-1">Price Matrix Forms</h1>
+        </div>
+    );
 }
 
 export default App;
