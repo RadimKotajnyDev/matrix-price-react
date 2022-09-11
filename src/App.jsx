d6679baf5cbae70221c5acbd22858b9ea08cc492
Radim Kotajny <67335383+RadimKotajnyDev@users.noreply.github.com>
2022-09-11 14:52:20 +0200

    Fields are sorted to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import "./App.css";
+import Field1 from "./components/Field1";
+import Field2 from "./components/Field2";
+import Field3 from "./components/Field3";
+import Field4 from "./components/Field4";
+import Field5 from "./components/Field5";
+import Field6 from "./components/Field6";
 
 /*
 Rule[] - kolekce pravidel (rules) viz níže
@@ -32,69 +38,9 @@ function App() {
     return (
         <div className="text-center mt-10 font-Inter text-3xl">
             <h1 className="underline underline-offset-8 decoration-1 font-normal">Price Matrix Forms</h1>
-            <p className="font-thin text-sm p-2 text-white bg-black m-2 rounded-lg w-fit mt-5 ml-auto mr-auto block">Improve this page as you wish.</p>
-            <div className="m-12 border-2 rounded-lg shadow-xl w-fit ml-auto mr-auto block px-5">
-                <form className="max-w-lg m-5 p-2 ml-auto mr-auto block">
-                    <div className="flex flex-wrap -mx-3 mb-6">
-                        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="">
-                            label
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="" type="text" placeholder="..." />
-                        
-                        </div>
-                        <div className="w-full md:w-1/2 px-3">
-                        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="">
-                            label
-                        </label>
-                        <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="" type="text" placeholder="..." />
-                        </div>
-                    </div>
-                    
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                        <div className="w-full px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
-                                select
-                            </label>
-                            <div className="relative">
-                                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                <option>Option 1</option>
-                                <option>Option 2</option>
-                                <option>Option 3</option>
-                                </select>
-                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="flex flex-wrap -mx-3 mb-2">
-                        <div className="w-full px-3 mb-6 md:mb-0">
-                            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="grid-state">
-                                select
-                            </label>
-                            <div className="relative">
-                                <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                <option>Option 1</option>
-                                <option>Option 2</option>
-                                <option>Option 3</option>
-                                </select>
-                                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="mt-5">
-                        <input type="submit" value="submit" className="w-full cursor-pointer border-2 p-2 bg-slate-900 text-white rounded hover:opacity-75 duration-700"/>
-                    </div>
-
-                </form>
-            </div>
-            
-        
+            <p className="font-light text-sm p-2 bg-gray-200 text-gray-700 m-2 rounded w-fit mt-5 ml-auto mr-auto block">Improve
+                this page as you wish.</p>
+            <Field1 />
         </div>
     );
 }
